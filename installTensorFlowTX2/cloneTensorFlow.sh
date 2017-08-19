94632b2b72245c408d81091feea655ac7db81a78
Anderson Banihirwe <axbanihirwe@ualr.edu>
2017-08-19 15:25:45 -0500

    Add tensorflow build scripts

---
+++ installTensorFlowTX2/cloneTensorFlow.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# NVIDIA Jetson TX2
+# TensorFlow Installation
+# Install Tensorflow repository then
+# setup for compilation
+# This does not build tensorflow
+INSTALL_DIR=$PWD
+cd $HOME
+git clone https://github.com/tensorflow/tensorflow.git
+cd tensorflow
+git checkout v1.3.0
+patch -p1 < $INSTALL_DIR/patches/tensorflow.patch
+
+
+
+
